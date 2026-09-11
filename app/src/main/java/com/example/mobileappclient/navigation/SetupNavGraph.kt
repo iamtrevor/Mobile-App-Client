022ada12022917f19c73a3d1edd858fba54fc7c0
iamtrevor <trevor.k.gachagua@gmail.com>
2026-09-11 11:18:24 +0300

    Splash screen implementation

--- app/src/main/java/com/example/mobileappclient/navigation/SetupNavGraph.kt
+++ app/src/main/java/com/example/mobileappclient/navigation/SetupNavGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.mobileappclient.presentation.screen.splash.SplashScreen
 import com.example.mobileappclient.utils.Constants.DETAILS_ARGUMENTS_KEY
 
 @Composable
@@ -16,7 +17,7 @@ fun SetupNavGraph(navController: NavHostController){
     ){
 
         composable(route = Screen.Splash.route) {
-
+            SplashScreen(navController = navController)
         }
 
         composable(route = Screen.Welcome.route) {
---
+++ app/src/main/java/com/example/mobileappclient/presentation/screen/splash/SplashScreen.kt
@@ -0,0 +1,80 @@
+package com.example.mobileappclient.presentation.screen.splash
+
+
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavHostController
+import com.example.mobileappclient.R
+import com.example.mobileappclient.ui.theme.Purple500
+import com.example.mobileappclient.ui.theme.Purple700
+
+
+@Composable
+fun SplashScreen(navController : NavHostController){
+
+    //logic to rotate splash screen logo
+    val degrees = remember { Animatable(0f) }
+
+    LaunchedEffect( key1 = 1) {
+        degrees.animateTo(
+            targetValue = 360f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                delayMillis = 200
+            )
+        )
+    }
+
+
+    Splash(degrees = degrees.value)
+}
+
+
+@Composable
+fun Splash(degrees : Float){
+    if(isSystemInDarkTheme()){
+        Box(modifier = Modifier
+            .background(Color.Black)
+            .fillMaxSize(),
+            contentAlignment = Alignment.Center)
+        {
+
+            Image(
+                modifier = Modifier.rotate(degrees = degrees),
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "App logo"
+            )
+        }
+    }else{
+        Box(modifier = Modifier
+            .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
+            .fillMaxSize(),
+            contentAlignment = Alignment.Center)
+        {
+
+            Image(
+                modifier = Modifier.rotate(degrees = degrees),
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "App logo"
+            )
+        }
+    }
+}
+
+
+
