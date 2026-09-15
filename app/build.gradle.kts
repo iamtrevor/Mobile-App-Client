9afc1cef4b58eb943f00c5b6a0bec51788d7ba09
iamtrevor <trevor.k.gachagua@gmail.com>
2026-09-15 11:26:07 +0300

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,9 @@ dependencies {
 
     implementation(platform(libs.androidx.compose.bom))
 
+    implementation(libs.androidx.compose.material.icons.extended)
+
+
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.compose.material3)
