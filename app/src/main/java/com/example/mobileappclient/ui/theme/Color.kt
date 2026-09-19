60bebf2f53b7909f344c3e8f25633155ae92b093
iamtrevor <trevor.k.gachagua@gmail.com>
2026-09-19 13:30:21 +0300

    initial commit

---
+++ app/src/main/java/com/example/mobileappclient/ui/theme/Color.kt
@@ -0,0 +1,70 @@
+package com.example.mobileappclient.ui.theme
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Purple700 = Color(0xFF3700B3)
+val Purple500 = Color(0xFF6200EE)
+
+val LightGray = Color(0xFFD8D8D8)
+val DarkGray = Color(0xFF2A2A2A)
+val StarColor = Color(0xFFFFC94D)
+
+//extension class for welcome screen background color
+val ColorScheme.welcomeScreenBackGroundColor
+    @Composable
+    get() = if(isSystemInDarkTheme()) Color.Black else Color.White
+
+
+val ColorScheme.titleColor
+    @Composable
+    get() = if(isSystemInDarkTheme()) LightGray else DarkGray
+
+
+val ColorScheme.descriptionColor
+    @Composable
+    get() = if(isSystemInDarkTheme()) LightGray.copy(alpha = 0.5f) else DarkGray.copy(alpha = 0.5f)
+
+
+
+
+val ColorScheme.activeIndicatorColor
+    @Composable
+    get() = if(isSystemInDarkTheme()) Purple700 else Purple500
+
+
+val ColorScheme.isInactiveIndicatorColor
+    @Composable
+    get() = if(isSystemInDarkTheme()) DarkGray else LightGray
+
+
+val ColorScheme.buttonBackgroundColors
+    @Composable
+    get() = if(isSystemInDarkTheme()) Purple700 else Purple500
+
+
+
+
+val ColorScheme.topAppBarContentColor : Color
+    @Composable
+    get() = if(isSystemInDarkTheme()) LightGray else Color.White
+
+
+val ColorScheme.topAppBarBackgroundColor : Color
+    @Composable
+    get() = if(isSystemInDarkTheme()) Color.Black else Purple500
+
+
+
