bc2244d55c87c590283391c5d71638786aee26cc
iamtrevor <trevor.k.gachagua@gmail.com>
2026-09-21 11:44:22 +0300

    changes

--- app/src/main/java/com/example/mobileappclient/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/mobileappclient/presentation/screen/home/HomeScreen.kt
@@ -1,18 +1,16 @@
 package com.example.mobileappclient.presentation.screen.home
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.padding
+
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.mobileappclient.presentation.common.ListContent
-import com.example.mobileappclient.presentation.components.RatingWidget
-import com.example.mobileappclient.ui.theme.LARGE_PADDING
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+
+
+
 @Composable
 fun HomeScreen(
     navController: NavHostController,
@@ -26,12 +24,15 @@ fun HomeScreen(
         topBar = {
             HomeTopBar(onSearchedClicked = {})
         },
-        content = {
+        content = { paddingValues ->
             ListContent(
                 heroes = allHeroes,
-                navController = navController
+                navController = navController,
+                paddingValues = paddingValues
             )
         }
     )
 
 }
+
+
